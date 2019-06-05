2f3f618898f93a15f1608004c43dd26c3853fe33
Diseño1 <Diseo1@1131MIRB01.presidencia.gob.mx>
2019-06-05 17:39:11 -0500

    Cambio de columnas en buscador y tamaño de body?

--- recursos/js/main.js
+++ recursos/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     $(window).on('scroll', function () {
         var scrollTop = $(window).scrollTop();
         if (scrollTop > 100) {
-            $('.memoria-buscador').stop().animate({height: "100px"},15);
+            $('.altura-variable').stop().animate({height: "100px"},15);
         }
         else {
-            $('.memoria-buscador').stop().animate({height: "200px"},15);   
+            $('.altura-variable').stop().animate({height: "200px"},15);   
         }
     });
 
